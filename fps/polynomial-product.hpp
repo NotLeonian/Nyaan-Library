6bc03f09a0a224ed0b8c699913c1e77bdafa2aff
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-06-14 14:30:34 +0900

    polish `Pi`

--- fps/polynomial-product.hpp
+++ fps/polynomial-product.hpp
@@ -3,9 +3,10 @@
 
 template <typename fps>
 fps Pi(vector<fps> v) {
-  if ((int)v.size() == 0) return fps{1};
+  if (v.empty()) return fps{1};
   while ((int)v.size() >= 2) {
     vector<fps> nx;
+    nx.reserve((v.size() + 1) / 2);
     for (int i = 0; i + 1 < (int)v.size(); i += 2)
       nx.push_back(v[i] * v[i + 1]);
     if (v.size() % 2) nx.push_back(v.back());
